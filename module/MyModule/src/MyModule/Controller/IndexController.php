1651b056d9c66d9363c011ec648c7e720b656787
alexsher <alex@sherman-site.com>
2012-11-25 02:40:06 +0200

    Change-Id: I4d0afb5083af735b88f968eea04ec77e54cd72b1

---
+++ module/MyModule/src/MyModule/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+namespace MyModule\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController {
+
+	public function indexAction() {
+		return new ViewModel(array(
+			'welcome_text' => "Hello World!",				
+		));
+	}
+}
