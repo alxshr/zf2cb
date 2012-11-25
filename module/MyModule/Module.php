75d7147c824a1dbf571cde6b0d9f25a8781f5c0e
alexsher <alex@sherman-site.com>
2012-11-25 02:22:14 +0200

    Change-Id: Ib6dc982842e10b52fbb3bb93c83914209f70df2b

--- module/MyModule/Module.php
+++ module/MyModule/Module.php
@@ -1,31 +1,35 @@
 <?php
+
 namespace MyModule;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
 
-class Module implements ServiceProviderInterface, AutoloaderProviderInterface, ConfigProviderInterface {
+class Module implements 	ServiceProviderInterface, 
+							AutoloaderProviderInterface, 
+							ConfigProviderInterface {
+	
 	public function getAutoloaderConfig() {
 		return array (
-				'Zend\Loader\ClassMapAutoloader' => array (
-						__DIR__ . '/autoload_classmap.php'
-				),
-				'Zend\Loader\StandardAutoloader' => array (
-						'namespaces' => array (
-								__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
-						)
-				)
+			'Zend\Loader\ClassMapAutoloader' => array (
+				__DIR__ . '/autoload_classmap.php' 
+			),
+			'Zend\Loader\StandardAutoloader' => array (
+				'namespaces' => array (
+				__NAMESPACE__ => __DIR__ . '/src/' . 
+											__NAMESPACE__ 
+				) 
+			) 
 		);
 	}
 	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
+		return include __DIR__ . 
+							'/config/module.config.php';
 	}
-	
 	public function getServiceConfig() {
 		return array (
-				'factories' => array (
-				)
+				'factories' => array () 
 		);
 	}
 }
